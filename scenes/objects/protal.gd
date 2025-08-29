f11abc281ffb5825147143fcf02e3e1950c647c2
G36maid <miku65434@gmail.com>
2025-08-29 08:52:34 +0800

    Add spawn point object and update levels and tileset

--- scenes/objects/protal.gd
+++ scenes/objects/protal.gd
@@ -7,7 +7,6 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("player") and next_scene_path != "":
-		# 找到 Main 並叫它換地圖
-		var main = get_tree().current_scene
-		if main.has_method("change_level"):
-			main.change_level(next_scene_path)
+		var game = get_tree().current_scene
+		if game.has_method("change_level"):
+			game.call_deferred("change_level", next_scene_path)
